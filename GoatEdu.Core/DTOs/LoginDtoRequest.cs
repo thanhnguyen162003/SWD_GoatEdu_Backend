bdd0f50bb91bf07763966c12fd2543318f399181
thanhnguyen <look1692003@gmail.com>
2024-05-18 13:46:26 +0700

    fix image can be null

--- GoatEdu.Core/DTOs/LoginDtoRequest.cs
+++ GoatEdu.Core/DTOs/LoginDtoRequest.cs
@@ -5,5 +5,7 @@ public class LoginDtoRequest
    public string Username { get; set; }
    public string Password { get; set; }
    public string Email { get; set; }
-   public string Picture { get; set; }
+   public string? Picture { get; set; }
+
+  
 }
