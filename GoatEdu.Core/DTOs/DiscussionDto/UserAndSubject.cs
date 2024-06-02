b76255303c5152fd3a3daa9cf8637ee8f2d3bc38
Minh Khang <khangminh530@gmail.com>
2024-06-02 21:11:55 +0700

    fak u Thanh

--- GoatEdu.Core/DTOs/DiscussionDto/UserAndSubject.cs
+++ GoatEdu.Core/DTOs/DiscussionDto/UserAndSubject.cs
@@ -5,6 +5,7 @@ public class UserAndSubject
     public Guid UserId { get; set; }
     public string? UserName { get; set; }
     public string? FullName { get; set; }
+    public string? UserImage { get; set; }
     public Guid SubjectId { get; set; }
     public string? SubjectName { get; set; }
 }
