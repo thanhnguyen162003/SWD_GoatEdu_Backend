82a43314ae19dd2be9933740c2a2fc50d382231d
thanhnguyen <look1692003@gmail.com>
2024-06-29 09:08:09 +0700

    flashcard update

--- GoatEdu.API/Response/FlashcardUpdateModel.cs
+++ GoatEdu.API/Response/FlashcardUpdateModel.cs
@@ -4,4 +4,5 @@ public class FlashcardUpdateModel
 {
     public string? flashcardName { get; set; }
     public string? flashcardDescription { get; set; }
+    public string? status { get; set; }
 }
