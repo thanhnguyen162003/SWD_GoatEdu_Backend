61ec221f7758c81693530e43e07a4992e1aa1f78
Minh Khang <khangminh530@gmail.com>
2024-07-05 19:34:19 +0700

    remove userId in notequery

--- GoatEdu.Core/QueriesFilter/NoteQueryFilter.cs
+++ GoatEdu.Core/QueriesFilter/NoteQueryFilter.cs
@@ -2,7 +2,6 @@ namespace GoatEdu.Core.QueriesFilter;
 
 public class NoteQueryFilter
 {
-    public Guid user_id { get; set; }
     public string sort { get; set; } = "date";
     public string sort_direction { get; set; } = "desc";
     public string? search { get; set; }
